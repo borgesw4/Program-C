4e3469f2d397629d876f54692898f1c28f8417c0
borgesw4 <borgesw4@outlook.com>
2021-03-29 16:07:10 -0300

    Create Class_02.c

---
+++ Class_02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int i=0; 
+	int j=0;
+	printf("Digite o primeiro valor na tela:\n");
+	scanf("%d",&i);
+	printf("Digite proximo valor na tela:\n");
+	scanf("%d",&j);
+	printf("Resultado: %d \n",i+j);
+	
+	system ("pause");
+}
